e944df3481233f2118a74e212a4403f252f7b318
micdoh <mic.d.doh@gmail.com>
2023-01-11 22:30:01 +0000

    Linear schedule no longer default

--- util_funcs.py
+++ util_funcs.py
@@ -1,7 +1,7 @@
 import gym
 import numpy as np
 from itertools import islice
-from typing import Generator, Any
+from typing import Generator, Any, Callable
 
 
 def get_nth_item(gen: Generator, n: int) -> Any:
@@ -38,3 +38,23 @@ def make_env(env_id, seed, **kwargs):
         return env
 
     return _init
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
