27dc4e3f4036ef3176d804a4fb814abec05a483f
micdoh <mic.d.doh@gmail.com>
2023-05-03 22:19:52 +0100

    run_heuristics.sh

---
+++ run_heuristics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the lists of parameters
+PH=(ff fdl msp_ef)
+NH=(nsc calrc tmr)
+
+# Loop over all combinations of PH and NH and run the command
+for p in ${PH[@]}; do
+  for n in ${NH[@]}; do
+    # Replace PH and NH in the command string with the current values of p and n
+    cmd="python3 eval_heuristic.py --env_file ./config/agent_conus.yaml --output_file ./eval/${n}_${p}_conus.csv --node_heuristic $n --path_heuristic $p --min_load 400 --max_load 1000 --load_step 100 --num_episodes 1"
+    echo "Running command: $cmd"
+    eval $cmd
+  done
+done
