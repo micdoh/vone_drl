42d4262c6da5d923f0d688186149b20eb7b5c260
micdoh <mic.d.doh@gmail.com>
2023-05-03 23:18:39 +0100

    New conus run commands

--- run_heuristics.sh
+++ run_heuristics.sh
@@ -8,7 +8,7 @@ NH=(nsc calrc tmr)
 for p in ${PH[@]}; do
   for n in ${NH[@]}; do
     # Replace PH and NH in the command string with the current values of p and n
-    cmd="python3 eval_heuristic.py --env_file ./config/agent_conus.yaml --output_file ./eval/${n}_${p}_conus.csv --node_heuristic $n --path_heuristic $p --min_load 400 --max_load 1000 --load_step 100 --num_episodes 1"
+    cmd="python3 eval_heuristic.py --env_file ./config/agent_conus.yaml --output_file ./eval/${n}_${p}_conus.csv --node_heuristic $n --path_heuristic $p --min_load 80 --max_load 100 --load_step 10 --num_episodes 1"
     echo "Running command: $cmd"
     eval $cmd
   done
